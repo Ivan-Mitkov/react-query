be2aafac0ebd1ffb7f58e9b8505cbf766e4f4d40
Ivan Mitkov <ivan.mitkov@gmail.com>
2021-09-16 18:09:30 +0300

    useQuery with params

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,7 @@
+import React from "react";
+import { useQuery } from "react-query";
 async function fetchComments(postId) {
+  console.log(postId);
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
   );
@@ -23,7 +26,31 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { id } = post;
+  const { data, isLoading, isError, error } = useQuery(
+    /**
+     * https://react-query.tanstack.com/guides/query-keys
+     * When a query needs more information to uniquely describe its data, you can use an array with a string and any number of serializable objects to describe it. This is useful for:
+
+      Hierarchical or nested resources
+      It's common to pass an ID, index, or other primitive to uniquely identify the item
+      Queries with additional parameters
+      It's common to pass an object of additional options
+     */
+    ["comments", id],
+    () => fetchComments(id),
+    {
+      staleTime: 1000,
+      cacheTime: 1000,
+    }
+  );
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+  if (isError) {
+    return <h3>{error.message}</h3>;
+  }
 
   return (
     <>
